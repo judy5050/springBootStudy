8c6610c48d8e7aa78a6139b52ebaaf2be16b9886
judy5050 <68409061+judy5050@users.noreply.github.com>
2021-06-24 03:18:09 +0900

    스프링 컨테이너와 스프칭 빈에 대한 공부 applicationContext 를 사용한 스프링 컨테이너 생성법 익힘 컨테이너에 등록된 빈에 대한 조회 방법 익힘 스프링 빈 조회에서 상속관계에 대해 알아봄  -최상위 빈 조회시 상속된 모든 빈이 호출된다. BeanFactory와 ApplicationContext의 차이

--- spring-core/core/src/main/java/hello/core/MemberApp.java
+++ spring-core/core/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,38 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
+    /**
+     * AppConfig 사용방식
+     */
+//    public static void main(String[] args) {
+//        AppConfig appConfig=new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        //의존성 관계 주입 전 코드
+////        MemberService memberService=new MemberServiceImpl();
+//        Member member = new Member(1L, "memberA", Grade.VIP);
+//        memberService.join(member);
+//
+//        Member findMember = memberService.findMember(1L);
+//        System.out.println("findMember = " + findMember.getName());
+//        System.out.println("member = " + member.getName());
+//    }
+
+    /**
+     * 스프링 사용 버전
+     */
     public static void main(String[] args) {
-        MemberService memberService=new MemberServiceImpl();
+
+
+        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
+
 
         Member findMember = memberService.findMember(1L);
         System.out.println("findMember = " + findMember.getName());
--- spring-core/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ spring-core/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,7 +1,9 @@
 package hello.core.member;
 
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,7 +12,14 @@ public class MemberServiceTest {
 
     //추상화에도 의존하고 구체화에도 의존
     //즉 ,DIP를 어긴다.
-    MemberService memberService=new MemberServiceImpl();
+//    MemberService memberService=new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig=new AppConfig();
+        memberService= appConfig.memberService();
+    }
 
     @Test
     void join(){
