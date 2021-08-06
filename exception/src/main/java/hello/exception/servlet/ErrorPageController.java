d8c83857e9fa19de776e47fca56d3b148c3e4593
judy5050 <68409061+judy5050@users.noreply.github.com>
2021-08-06 12:20:51 +0900

    Api 예외처리 - 스프링 부트 기본 오류 처리 방법 - HandlerExceptionResolver을 통한 예외 처리 방법 학습 - 스프링이 제공하는 ExceptionResolver 3가지의 우선순위 및 특징에 대해 공부   -ExceptionHandlerExceptionResolver   -ResponseStatusExceptionResolver   -DefaultHandlerExceptionResolver

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -2,11 +2,17 @@ package hello.exception.servlet;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -26,8 +32,22 @@ public class ErrorPageController {
         return  "error-page/404";
     }
 
-    @RequestMapping("/error-page/500")
-    public String errorPage500(HttpServletRequest request, HttpServletResponse response){
+    @RequestMapping(value = "/error-page/500",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String,Object>> errorPage500(HttpServletRequest request, HttpServletResponse response){
+        log.info("API error Page 500");
+
+
+        Map<String,Object>result = new HashMap<>();
+        Exception ex = (Exception)request.getAttribute(ERROR_EXCEPTION);
+        result.put("status",request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message",ex.getMessage());
+
+        Integer statusCode = (Integer)request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+    }
+
+    @RequestMapping(value = "/error-page/500")
+    public String errorPage500Api(HttpServletRequest request, HttpServletResponse response){
         log.info("error Page 500");
         printErrorInfo(request);
         return  "error-page/500";
