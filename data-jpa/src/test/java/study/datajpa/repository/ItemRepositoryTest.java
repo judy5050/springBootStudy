f5028ff6e7f7ed34b90de4414e143d6a42adba21
judy5050 <qkrgywjd456@naver.com>
2022-06-08 01:51:44 +0900

    init: jdbc study

---
+++ data-jpa/src/test/java/study/datajpa/repository/ItemRepositoryTest.java
@@ -0,0 +1,24 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Item;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@Rollback(value = false)
+@Transactional
+class ItemRepositoryTest {
+
+    @Autowired ItemRepository itemRepository;
+
+    @Test
+    public void save(){
+        Item item = new Item("A");
+    }
+}
